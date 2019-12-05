4bdf392bcc99aeaca5fd5e607f305c1f9f2bc0b0
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-04 18:05:44 -0800

    try builtin types & casters

--- src/app/models/bad-todo.ts
+++ src/app/models/bad-todo.ts
@@ -1,7 +1,12 @@
+import { Latitude, Longitude } from 'io-interface';
+
 export interface BadTodo {
   user_id: number;
   id: string;
   title: string;
   description: string;
   completed: boolean;
+  created_at: Date;
+  lat: Latitude;
+  lng: Longitude;
 }
